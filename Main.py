6318f750f8d5c1f57edfbada816ec844179703b4
jhavens <jhavens12@gmail.com>
2018-05-04 14:24:24 -0400

    label1

--- Main.py
+++ Main.py
@@ -12,6 +12,7 @@ def vis(w,h):
     vis['x_margin'] = 15
     vis['y_margin'] = 20
 
+    #SUBVIEWS
     vis['psub_h'] = h*.33
     vis['psub_w'] = w - (vis['x_margin']*2)
     vis['psub_x'] = vis['x_margin']
@@ -22,27 +23,32 @@ def vis(w,h):
     vis['csub_x'] = vis['x_margin']
     vis['csub_y'] = vis['y_margin'] + vis['psub_h'] + (vis['y_margin']/2) #y margin, height of psub, y margin
 
-    vis['fsub_h'] = h*.2
+    vis['fsub_h'] = h*.26
     vis['fsub_w'] = w-(vis['x_margin']*2)
     vis['fsub_x'] = vis['x_margin']
     vis['fsub_y'] = vis['csub_y'] + vis['csub_h'] + (vis['y_margin']/2) #y margin, height of psub, y margin
+
+    #LABELS
+    vis['psub_title_h'] = 15
+    vis['psub_title_w'] = vis['psub_w']
+    vis['psub_title_x'] = 0
+    vis['psub_title_y'] = 0
 
     return vis
 
 vis = vis(w,h)
 
-#past subview
+#SUBVIEWS
 psubview = ui.View(frame=(vis['psub_x'], vis['psub_y'], vis['psub_w'], vis['psub_h']), background_color = 'black')
-
-#current subview
 csubview = ui.View(frame=(vis['csub_x'], vis['csub_y'], vis['csub_w'], vis['csub_h']), background_color = 'blue')
-
 fsubview = ui.View(frame=(vis['fsub_x'], vis['fsub_y'], vis['fsub_w'], vis['fsub_h']), background_color = 'red')
-
-
 view.add_subview(psubview)
 view.add_subview(csubview)
 view.add_subview(fsubview)
 
+#LABELS
+title
+ptitle = ui.Label(name = 'ptitle', bg_color ='yellow', frame = (vis['psub_title_x'], vis['psub_title_y'], vis['psub_title_w'], vis['psub_title_h']))
+label.text = "ptitle"
 
 view.present(style='sheet', hide_title_bar=True)

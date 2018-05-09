bd28f49fdf7795f3e4256fc0aa43a4dc948c569e
jhavens <jhavens12@gmail.com>
2018-05-09 09:27:34 -0400

    fix

--- Main.py
+++ Main.py
@@ -198,7 +198,7 @@ def generate_psubview(psubview,csubview,pseg_info): #give the subview and list o
     #subtitles
     psubtitle_title = ui.Label(name = 'psubtitle_title', bg_color ='black', frame = (vis['psubtitle_title_x'], vis['psubtitle_title_y'], vis['psubtitle_title_w'], vis['psubtitle_title_h']))
     psubtitle_title.text = pseg_info['subtitle_title']
-    psubtitle_title.alignment = 2 #1 is center, 2 is right, 3
+    psubtitle_title.alignment = 1 #1 is center, 2 is right, 3
     psubtitle_title.text_color = 'white'
     #psubtitle_title.font =  ('<system-bold>',14)
     psubview.add_subview(psubtitle_title)
@@ -206,7 +206,7 @@ def generate_psubview(psubview,csubview,pseg_info): #give the subview and list o
     psubtitle_value = ui.Label(name = 'psubtitle_value', bg_color ='black', frame = (vis['psubtitle_value_x'], vis['psubtitle_value_y'], vis['psubtitle_value_w'], vis['psubtitle_value_h']))
     psubtitle_value.text = pseg_info['subtitle_value']
     psubtitle_value.text_color = '#4286f4'
-    psubtitle_value.alignment = 3 #1 is center
+    psubtitle_value.alignment = 1 #1 is center
     psubview.add_subview(psubtitle_value)
 
     #box titles

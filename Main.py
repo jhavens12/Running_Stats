38442b40060f9d50b475567e770042e0b21635c6
jhavens <jhavens12@gmail.com>
2018-05-17 09:07:56 -0400

    fix

--- Main.py
+++ Main.py
@@ -444,7 +444,7 @@ def generate_fsubview(fsubview,fseg_info):
         label_title = ui.Label(name = label, bg_color = 'black', frame = (vis['fbox_values_x'], vis['fbox_values_y'], vis['fbox_values_w'], vis['fbox_values_h']) )
         label_title.text = label
         label_title.alignment = 1
-        label_title.font =  ('<system>',12)
+        label_title.font =  ('<system>',10)
         label_title.text_color = '#4286f4'
         #label_title.border_color = 'black'
         #label_title.border_width = 0
@@ -471,7 +471,7 @@ def generate_fsubview(fsubview,fseg_info):
         label_title = ui.Label(name = label, bg_color = 'black', frame = (vis['frbox_values_x'], vis['frbox_values_y'], vis['frbox_values_w'], vis['frbox_values_h']) )
         label_title.text = label
         label_title.alignment = 1
-        label_title.font =  ('<system>',12)
+        label_title.font =  ('<system>',10)
         label_title.text_color = '#4286f4'
         #label_title.border_color = 'black'
         #label_title.border_width = 0

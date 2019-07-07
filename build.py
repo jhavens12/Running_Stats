e41586233bd0252047cce1e55ec963d8c0260d24
Jonathan Havens <jhavens12@gmail.com>
2019-07-07 18:42:27 -0400

    Update build.py

--- build.py
+++ build.py
@@ -395,7 +395,7 @@ def weekly(current_info):
             #added
             mmw_dt = weekly_dict[most_miles_week]['datetime']
             mmw_ds = datetime.datetime.now() - mmw_dt
-            if int(mmw_ds) < 0: #fix negative days if done within 24 hours
+            if float(mmw_ds) < 0: #fix negative days if done within 24 hours
                 mmw_ds = 0
 
     main_dict = {}
